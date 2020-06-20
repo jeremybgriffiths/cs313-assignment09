3ca47374a368bfa26a2ffc227869d787351ff04f
jeremybgriffiths <gri18025@byui.edu>
2020-06-19 22:26:36 -0600

    Added week 9 assignment

--- node-js-getting-started/index.js
+++ node-js-getting-started/index.js
@@ -2,61 +2,139 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// tell it to use the public directory as one where static files live
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// set up a rule that says requests to "/math" should be handled by the
-// handleMath function below
-app.get('/math', handleMath);
+app.get('/postal-calculator', getData);
 
-// start the server listening
-app.listen(port, function() {
-  console.log('Node app is running on port', port);
+app.listen(port, function () {
+	console.log('Node app is running on port', port);
 });
 
 
-/**********************************************************************
- * Ideally the functions below here would go into a different file
- * but for ease of reading this example and seeing all of the pieces
- * they are listed here.
- **********************************************************************/
+function getData(request, response) {
+	const weight = request.query.weight;
+	const type = request.query.type;
 
-function handleMath(request, response) {
-	const operation = request.query.operation;
-	const operand1 = Number(request.query.operand1);
-	const operand2 = Number(request.query.operand2);
-
-	// TODO: Here we should check to make sure we have all the correct parameters
-
-	computeOperation(response, operation, operand1, operand2);
+	calculateRate(response, weight, type);
 }
 
-function computeOperation(response, op, left, right) {
-	op = op.toLowerCase();
-
-	let result = 0;
-
-	if (op == "+") {
-		result = left + right;
-	} else if (op == "-") {
-		result = left - right;		
-	} else if (op == "X") {
-		result = left * right;
-	} else if (op == "/") {
-		result = left / right;
-	} else {
-		// It would be best here to redirect to an "unknown operation"
-		// error page or something similar.
+function calculateRate(response, weight, type) {
+	let total = 0;
+	switch (type) {
+		case "Letters (Stamped)":
+			total = calculateLetterStamped(weight);
+			break;
+		case "Letters (Metered)":
+			total = calculateLetterMetered(weight);
+			break;
+		case "Large Envelopes (Flats)":
+			total = calculateLargeEnvelope(weight);
+			break;
+		case "First-Class Package Service—Retail":
+			total = calculateFirstClassPackage(weight);
+			break;
+		default:
+			total = "Invalid Type";
+			break;
 	}
 
 	// Set up a JSON object of the values we want to pass along to the EJS result page
-	const params = {operation: op, left: left, right: right, result: result};
+	const params = {
+		weight: weight,
+		type: type,
+		total: total
+	};
 
 	// Render the response, using the EJS page "result.ejs" in the pages directory
 	// Makes sure to pass it the parameters we need.
-	response.render('pages/result', params);
+	response.render('pages/rate-details.ejs', params);
+}
+
+function calculateLetterStamped(weight) {
+	let total = 0;
+	if (weight <= 1) {
+		total = 0.55;
+	} else if (weight <= 2) {
+		total = 0.70;
+	} else if (weight <= 3) {
+		total = 0.85;
+	} else if (weight <= 3.5) {
+		total = 1.00;
+	} else {
+		total = "Invalid Weight";
+	}
+
+	return total;
+}
+
+function calculateLetterMetered(weight) {
+	let total = 0;
+	if (weight <= 1) {
+		total = 0.50;
+	} else if (weight <= 2) {
+		total = 0.65;
+	} else if (weight <= 3) {
+		total = 0.80;
+	} else if (weight <= 3.5) {
+		total = 0.95;
+	} else {
+		total = "Invalid Weight";
+	}
+
+	return total;
+}
+
+function calculateLargeEnvelope(weight) {
+	let total = 0;
+	if (weight <= 1) {
+		total = 1.00;
+	} else if (weight <= 2) {
+		total = 1.20;
+	} else if (weight <= 3) {
+		total = 1.40;
+	} else if (weight <= 4) {
+		total = 1.60;
+	} else if (weight <= 5) {
+		total = 1.80;
+	} else if (weight <= 6) {
+		total = 2.00;
+	} else if (weight <= 7) {
+		total = 2.20;
+	} else if (weight <= 8) {
+		total = 2.40;
+	} else if (weight <= 9) {
+		total = 2.60;
+	} else if (weight <= 10) {
+		total = 2.80;
+	} else if (weight <= 11) {
+		total = 3.00;
+	} else if (weight <= 12) {
+		total = 3.20;
+	} else if (weight <= 13) {
+		total = 3.40;
+	} else {
+		total = "Invalid Weight";
+	}
+
+	return total;
+}
+
+function calculateFirstClassPackage(weight) {
+	var total = 0;
+	if (weight <= 4) {
+		total = 3.80;
+	} else if (weight >= 5 && weight <= 8) {
+		total = 4.60;
+	} else if (weight >= 9 && weight <= 12) {
+		total = 5.30;
+	} else if (weight == 13) {
+		total = 5.90;
+	} else if (weight > 13) {
+		total = "Invalid Weight";
+	}
+
+	return total;
 }
